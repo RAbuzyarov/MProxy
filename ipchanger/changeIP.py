cfba95c0c87c23831ea8c31e8fd8bb13f3944dda
Renat Abuzyarov <RAbuzyarov@gmail.com>
2023-02-13 23:50:00 +0300

    доработана функция проверки полученного IP-адреса. Если модем после перезапуска получил IP-адрес, который уже есть у какого-либо из других модемов, то модем отправляется за новым IP-адресом.

--- ipchanger/changeIP.py
+++ ipchanger/changeIP.py
@@ -3,20 +3,25 @@
 import os
 import subprocess
 import time
-
+import json
 from huawei_lte_api.Client import Client
 from huawei_lte_api.Connection import Connection
 
-max_ip_life_time = 300 # время жизни IP-адреса модема
-wait_time = 20 # количество секунд ожидания поднятия модема после смены IP
+max_ip_life_time = 300  # время жизни IP-адреса модема
+wait_time = 20  # количество секунд ожидания поднятия модема после смены IP
+
 
 # Функция вывода модема из пула балансировки haproxy
 def removeModemFromPull(modem):
-    result = os.system('echo "set server ' + modem.rstrip() + '/' + modem.rstrip() + ' state drain" | socat stdio tcp4-connect:127.0.0.1:1350')  # выведем текущий сервер из пула haproxy
+    result = os.system(
+        'echo "set server ' + modem + '/' + modem + ' state drain" | socat stdio tcp4-connect:127.0.0.1:1350')  # выведем текущий сервер из пула haproxy
+
 
 # Функция возврата модема в пул балансировки haproxy
 def returnModemToPull(modem):
-    result = os.system('echo "set server ' + modem.rstrip() + '/' + modem.rstrip() + ' state ready" | socat stdio tcp4-connect:127.0.0.1:1350')  # вернем текущий сервер в пул haproxy
+    result = os.system(
+        'echo "set server ' + modem + '/' + modem + ' state ready" | socat stdio tcp4-connect:127.0.0.1:1350')  # вернем текущий сервер в пул haproxy
+
 
 def isModemNeedChangeIP(modem, client) -> bool:
     if int(client.monitoring.traffic_statistics().get('CurrentConnectTime')) >= max_ip_life_time:
@@ -24,18 +29,19 @@ def isModemNeedChangeIP(modem, client) -> bool:
     else:
         return False
 
+
 # Функция смены IP-адреса модема. При успешном выполнении возвращает True, иначе False
 def changeModemIP(modem, client) -> bool:
     # сменим IP-адрес модему
-    client.net.set_net_mode("7FFFFFFFFFFFFFFF", "3FFFFFFF", "02") # 3G
-    client.net.set_net_mode("7FFFFFFFFFFFFFFF", "3FFFFFFF", "03") # LTE
+    client.net.set_net_mode("7FFFFFFFFFFFFFFF", "3FFFFFFF", "02")  # 3G
+    client.net.set_net_mode("7FFFFFFFFFFFFFFF", "3FFFFFFF", "03")  # LTE
     # подождем wait_time секунд, чтобы модем подключился к сети. Если за половину отведенного времени не подключился,
     # то делаем модему выкл/вкл и ждем оставшуюся половину времени
     for i in range(wait_time):
         status = client.monitoring.status()
         if status.get('ConnectionStatus') == '901':
             break
-        if i >= wait_time/2:
+        if i >= wait_time / 2:
             client.dial_up.set_mobile_dataswitch(0)
             time.sleep(1)
             client.dial_up.set_mobile_dataswitch(1)
@@ -47,28 +53,40 @@ def changeModemIP(modem, client) -> bool:
     else:
         return False
 
+
 def checkModemConnection(modem):
-    command1 = "echo \"show servers state\" | socat stdio tcp4-connect:127.0.0.1:1350 | grep -E \"(" + modem.rstrip() + ".*){2}\" | cut -d \" \" -f 5,19"
+    command1 = "echo \"show servers state\" | socat stdio tcp4-connect:127.0.0.1:1350 | grep -E \"(" + modem + ".*){2}\" | cut -d \" \" -f 5,19"
     response1 = subprocess.check_output(command1, shell=True, text=True)
     proxy = response1.split()
     command2 = "curl -sx http://" + proxy[0] + ":" + proxy[1] + " http://2ip.ru"
-    response2 = subprocess.check_output(command2, shell=True, text=True)
-    if response2:
-        return True
-    else:
+    response2 = subprocess.check_output(command2, shell=True, text=True).rstrip()
+    if not response2:
         return False
+    elif not os.path.exists("externalIPs.txt"):
+        d= {modem: response2}
+    else:
+        with open('externalIPs.txt') as f1:
+            d: object = json.load(f1)
+        for key, value in d.items():
+            if value == response2:
+                return False
+        d[modem] = response2
+    with open('externalIPs.txt', 'w') as f1:
+        f1.write(json.dumps(d))
+    return True
+
 
 # Основной сценарий
 
 # получим список модемов из конфиг файла ltemodems.cfg
 with open('ltemodems.cfg', 'r') as f:
-    Modems = f.readlines()
+    Modems = list([line.rstrip() for line in f])
 
 # для каждого модема по списку, если после последней смены IP-адреса прошло более 5 минут, выполним вывод из пула haproxy, сменим IP-адрес,
 # удостоверимся, что модем точно подключен к интернету и что полученный IP-адрес уникален, и вернем его в пул.
 # Если модем не подключился или адрес не уникален, то повторим процедуру смены адреса (максимум 3 повтора).
 for modem in Modems:
-    with Connection("http://admin:Password01*@" + modem.rstrip()) as connection:
+    with Connection("http://admin:Password01*@" + modem) as connection:
         client = Client(connection)
         if not isModemNeedChangeIP(modem, client):
             break
@@ -80,5 +98,3 @@ for modem in Modems:
                     break
             else:
                 break
-
-
